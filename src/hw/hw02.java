09eb8d9eaa35854648443dac3e40af9d7883c5ee
kuchu13 <105021033@live.asia.edu.tw>
2016-10-30 21:15:28 +0800

    HW5 EX6 not yet

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -6,9 +6,17 @@ import java.util.Scanner;
 public class hw02 {
 
 	public static void main(String[] args) {
-		
 		Scanner scn = new Scanner(System.in);
-
+		
+		System.out.println("請輸入正整數n");
+		int a = scn.nextInt();
+		int n = 0;
+		int m = 0;
+		for (int i = 1; n <= a; i++) {
+			n = n + i;
+			m++;
+		}
+		System.out.println("m = " + (m - 1));
 	}
 
 }
